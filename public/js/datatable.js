ad395050bfbadcae65b7a77e5412c6b64d65c56a
Andres Escala <79557629+escalandres@users.noreply.github.com>
2023-10-10 22:18:17 -0600

    Iconos y ordenamiento agregados al datatable

--- public/js/datatable.js
+++ public/js/datatable.js
@@ -48,12 +48,14 @@ var table = $('#fileTable').DataTable({
     "columnDefs": [
         { "targets": 0, "className": "text-left" },
         { "targets": [1,2], "className": "text-center", "searchable": false },
+        { "targets": [3,2], "orderable": false },
         { "targets": 3, "className": "", "searchable": false, "visible": false },
         // {
         //     "targets": 0, // La columna que deseas ordenar de manera personalizada
         //     "type": 'folder-first' // Usa el tipo de ordenamiento personalizado 'folder-first'
         // }
-    ]
+    ],
+    "order": [[3,'asc'],[0,'asc']],
 });
 
 function construirTabla(tableId, folderData) {
@@ -83,66 +85,16 @@ function construirTabla(tableId, folderData) {
     agregarEventosRow()
     var elementoParaEliminar = document.getElementById("folderInfo"); // Reemplaza "miElemento" con el ID de tu elemento
     elementoParaEliminar.remove();
+    agregarIcono()
   }
-
-// function consultarCarpeta(){
-//     // Crear los datos que deseas enviar en el cuerpo de la solicitud POST
-//     const datos = {
-//         clave1: 'valor1'
-//     };
-//     // Realizar la solicitud POST
-//     fetch('/drive/mydrive', {
-//         method: 'POST',
-//         headers: {
-//         'Content-Type': 'application/json' // Establecer el tipo de contenido del cuerpo de la solicitud
-//         },
-//         body: JSON.stringify(datos) // Convertir los datos a formato JSON y establecerlos como cuerpo de la solicitud
-//     })
-//     .then(response => {
-//         // Manejar la respuesta de la solicitud
-//         if (response.ok) {
-//             return response.json(); // Si la respuesta es exitosa, convertir la respuesta a JSON
-//         } else {
-//             throw new Error('Error en la solicitud POST');
-//         }
-//     })
-//     .then(data => {
-//       // Manejar los datos de la respuesta
-//         console.log('Respuesta del servidor:', data);
-//         construirTabla('fileTable',data)
-        
-//     })
-//     .catch(error => {
-//       // Manejar errores de la solicitud
-//         console.error('Error:', error);
-//     });
-// }
 
 function consultarCarpeta(){
     try{
         var folderInfoDiv = document.getElementById("folderInfo");
         var texto = folderInfoDiv.textContent;
-        console.log(texto)
         // Analiza el texto JSON en un objeto JavaScript
         var folderObj = JSON.parse(texto);
-        console.log(folderObj)
         construirTabla('fileTable',folderObj)
-        // Verifica que tengas un objeto de la clase Folder
-        // if (folderObj instanceof Folder) {
-        //     // Itera a través de la lista de archivos dentro del objeto Folder
-        //     folderObj.Archivos.forEach(function(archivo) {
-        //         // Si deseas convertir los objetos Archivo a instancias de la clase Archivo, puedes hacerlo aquí
-        //         var archivoObj = new Archivo();
-        //         archivoObj.Name = archivo.Name;
-        //         archivoObj.ModificationDate = archivo.ModificationDate;
-        //         archivoObj.Extension = archivo.Extension;
-        //         archivoObj.Size = archivo.Size;
-            
-        //         // Ahora, archivoObj contiene un objeto de la clase Archivo
-        //         console.log(archivoObj);
-        //     });
-        //     construirTabla('fileTabla',folderObj)
-        // }
     }
     catch(err){
         console.error(err)
@@ -204,3 +156,159 @@ function esPdfOImagen(extension) {
 $('#div_visor').on('hidden.bs.modal', function (event) {
     visorPDF.src = "";
 });
+
+function agregarIcono(){
+    var miTabla = $('#fileTable').DataTable();
+    miTabla.rows().every(function() {
+        var iconClass = buscarIcono(this.data().Extension); // Encuentra la clase de icono según el valor de la columna "Extension"
+        var iconElement = `<i class="i-icon ${iconClass}"></i> `; // Elemento <i> con la clase de icono
+    
+        // Agrega el elemento <i> antes del texto en la primera celda de la fila actual
+        var primeraCelda = this.node().getElementsByTagName('td')[0];
+        primeraCelda.innerHTML = iconElement + primeraCelda.innerHTML;
+    });
+}
+
+function buscarIcono(fileExtension){
+    var icons = {
+        // base icons
+        'default': 'unknown-icon',
+        'folder': 'folder-icon',
+    
+        // tipos de archivo de imagen
+        '.jpg': 'img-icon',
+        '.jpeg': 'img-icon',
+        '.png': 'img-icon',
+        '.gif': 'gif-icon',
+        '.bmp': 'img-icon',
+        '.tiff': 'img-icon',
+        '.svg': 'svg-icon',
+        '.webp': 'img-icon',
+    
+        // tipos de archivo de audio
+        '.mp3': 'audio-icon',
+        '.m4a': 'audio-icon',
+        '.wav': 'audio-icon',
+        '.flac': 'audio-icon',
+    
+        // tipos de archivo de video
+        '.mp4': 'video-icon',
+        '.avi': 'video-icon',
+        '.mov': 'video-icon',
+        '.mkv': 'video-icon',
+    
+        // tipos de archivo de programación
+        '.js': 'js-icon',
+        '.json': 'code-icon',
+        '.css': 'css-icon',
+        '.html': 'html-icon',
+        '.php': 'code-icon',
+        '.py': 'code-icon',
+        '.java': 'code-icon',
+        '.cpp': 'code-icon',
+        '.cs': 'code-icon',
+        '.rb': 'code-icon',
+        '.pl': 'code-icon',
+        '.sh': 'code-icon',
+        '.sql': 'sql-icon',
+        '.c': 'code-icon',
+        '.scss': 'code-icon',
+    
+        // otros tipos de archivo comunes
+        '.doc': 'word-icon',
+        '.docx': 'word-icon',
+        '.pdf': 'pdf-icon',
+        '.postscript': 'svg-icon',
+        '.rtf': 'word-icon',
+        '.xls': 'excel-icon',
+        '.xlsx': 'excel-icon',
+        '.ppt': 'ppt-icon',
+        '.pptx': 'ppt-icon',
+        '.7z': 'compressed-icon',
+        '.tar': 'compressed-icon',
+        '.xz': 'compressed-icon',
+        '.zip': 'compressed-icon',
+        '.rar': 'compressed-icon',
+        '.xml': 'xml-icon',
+        '.txt': 'txt-icon',
+        // tipos de archivo de fuente de texto
+        '.ttf': 'font-icon',
+        '.otf': 'font-icon',
+        '.woff': 'font-icon',
+        '.woff2': 'font-icon',
+    
+        // specific mime type icons
+        '.rtf': 'word-icon',
+    
+        // other, extension-specific icons
+        '.accdb': 'database-icon',
+        '.apk': 'apk-icon',
+        '.app': 'unknown-icon',
+        '.as': 'page_white_actionscript-icon',
+        '.asp': 'code-icon',
+        '.aspx': 'code-icon',
+        '.bat': 'terminal-icon',
+        '.bz2': 'compressed-icon',
+    
+        '.cab': 'compressed-icon',
+        '.cfm': 'code-icon',
+        '.clj': 'code-icon',
+        '.cc': 'code-icon',
+        '.cgi': 'terminal-icon',
+        '.db': 'database-icon',
+        '.dbf': 'database-icon',
+        '.deb': 'compressed-icon',
+        '.dll': 'gear-icon',
+        '.dmg': 'drive-icon',
+        '.erb': 'code-icon',
+        '.exe': 'exe-icon',
+        '.fnt': 'font-icon',
+        '.gam': 'controller-icon',
+        '.gz': 'compressed-icon',
+        '.h': 'code-icon',
+        '.ini': 'gear-icon',
+        '.iso': 'cd-icon',
+        '.jar': 'compressed-icon',
+        '.jsp': 'code-icon',
+        '.lua': 'code-icon',
+        '.lz': 'compressed-icon',
+        '.lzma': 'compressed-icon',
+        '.m': 'code-icon',
+        '.map': 'map-icon',
+        '.msi': 'compressed-icon',
+        '.otf': 'font-icon',
+        '.pdb': 'database-icon',
+        '.php': 'code-icon',
+        '.pl': 'code-icon',
+        '.pkg': 'compressed-icon',
+        '.psd': 'img-icon',
+        '.py': 'code-icon',
+        '.rb': 'code-icon',
+        '.rm': 'video-icon',
+        '.rom': 'controller-icon',
+        '.rpm': 'compressed-icon',
+        '.sass': 'code-icon',
+        '.sav': 'controller-icon',
+    
+        '.srt': 'txt-icon',
+        '.tbz2': 'compressed-icon',
+        '.tgz': 'compressed-icon',
+        '.tlz': 'compressed-icon',
+        '.vb': 'code-icon',
+        '.vbs': 'code-icon',
+        '.xcf': 'img-icon',
+        '.yaws': 'code-icon'
+    };
+
+    if(fileExtension === "") return 'folder-icon'
+    // Busca el tipo de archivo en el objeto icons
+    const fileType = icons[fileExtension];
+
+    // Si se encuentra el tipo de archivo, devuelve su nombre de clase
+    if (fileType) {
+        return fileType;
+    } else {
+        // Si no se encuentra, devuelve 'unknown-icon' por defecto
+        return 'unknown-icon';
+    }
+}
