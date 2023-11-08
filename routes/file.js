953d3d357c0a4c7d5218077f13ce5b89249c4a78
Andres Escala <79557629+escalandres@users.noreply.github.com>
2023-11-07 22:12:18 -0600

    file router added

---
+++ routes/file.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+// Set up storage for uploaded files
+
+const currentFileURL = import.meta.url;
+// Convierte la URL del archivo en una ruta de sistema de archivos
+const currentFilePath = fileURLToPath(currentFileURL);
+// Obtiene el directorio del archivo actual
+const __dirname = dirname(currentFilePath);
+
+import { signup, login, logout, changeUserPassword, generateOTP, checkOTP, validateAuthToken, validateChangeToken } from '../controllers/user.js';
+
+const router = express.Router();
+
+router.get('/check-otp', validateAuthToken, (req,res)=>{
+    return res.sendFile(path.join(__dirname,'../','src','views','otp.html'));
+})
+
+router.get('/change-password', validateChangeToken, (req,res)=>{
+    return res.sendFile(path.join(__dirname,'src','views','change-password.html'));
+})
+
+// router.get('/change-password',  (req,res)=>{
+//     return res.sendFile(path.join(__dirname,'../','src','views','change.html'));
+// })
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+router.patch('/change-password', changeUserPassword);
+router.post('/generate-otp', generateOTP);
+router.post('/check-otp', checkOTP);
+// router.get('/')
+
+export default router;
